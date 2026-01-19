501a0d124660aa2b2c683f7f064ec39e8b9dee1d
ashonbaiju <ashonbaiju@gmail.com>
2026-01-19 12:25:30 -0500

    Added simple interest calculator script

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+
+read -p "Enter Principal: " p
+read -p "Enter Rate: " r
+read -p "Enter Time: " t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+
+echo "Simple Interest = $si"
